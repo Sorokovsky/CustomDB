0d01f44bfd52327c0bee1663ec8efc58f86bbf2e
Sorokovsky <Sorokovskys@ukr.net>
2024-12-13 14:50:01 +0200

    paths

---
+++ Database/Contracts/Paths.cs
@@ -0,0 +1,14 @@
+﻿namespace Database.Contracts;
+
+public static class Paths
+{
+    private const string BasePath = "database";
+    
+    public static string GetTablePath(string fileName) => $"{BasePath}/tables/{fileName}.dat";
+
+    public static string GetLastKeyPath(string fileName) => $"{BasePath}/lastKey/{fileName}.dat";
+
+    public static string GetPrimaryKeyPath(string fileName) => $"{BasePath}/keys/primary/{fileName}.dat";
+
+    public static string GetForeignPath(string fileName) => $"{BasePath}/keys/foreign/{fileName}.dat";
+}
---
+++ Database/Core/Repository.cs
@@ -0,0 +1,12 @@
+﻿namespace Database.Core;
+
+public class Repository<T> where T : class
+{
+    private LinkedList<T> _list = [];
+
+    public T Add(T item)
+    {
+        var result = _list.AddLast(item).Value;
+        return result;
+    }
+}
