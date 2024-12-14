0c6f23135119b042471ab7f7b09639736f6f2d6a
Sorokovsky <Sorokovskys@ukr.net>
2024-12-14 14:41:55 +0200

    remove

--- Database/Core/DbEvents.cs
+++ Database/Core/DbEvents.cs
@@ -4,9 +4,17 @@ public class DbEvents
 {
     public delegate void Operation(object data);
 
+    public delegate void ErrorHandler(string reason);
+
     public static event Operation PreCreated;
 
     public static event Operation Created;
+
+    public static event Operation PreRemoved;
+
+    public static event Operation Removed;
+
+    public static event ErrorHandler NotRemoved;
 
     public static void OnPreCreated(object data)
     {
@@ -16,5 +24,20 @@ public class DbEvents
     public static void OnCreated(object data)
     {
         Created?.Invoke(data);
+    }
+
+    public static void OnRemoved(object data)
+    {
+        Removed?.Invoke(data);
+    }
+
+    public static void OnPreRemoved(object data)
+    {
+        PreRemoved?.Invoke(data);
+    }
+
+    public static void OnNotRemoved(string reason)
+    {
+        NotRemoved?.Invoke(reason);
     }
 }
--- Database/Core/Repository.cs
+++ Database/Core/Repository.cs
@@ -1,14 +1,63 @@
-﻿namespace Database.Core;
+﻿using Database.Contracts;
+using Database.Services;
+
+namespace Database.Core;
 
 public class Repository<T> where T : class
 {
-    private readonly LinkedList<T> _list = [];
+    private readonly IStorage<LinkedList<T>> _storage;
+    private LinkedList<T> _list = [];
+
+    public delegate bool IsNeed(T item);
+
+    public IReadOnlyList<T> List => _list.ToList();
+
+    public Repository(string tableName)
+    {
+        _storage = new FileService<LinkedList<T>>(Paths.GetTablePath(tableName));
+        Load();
+    }
 
     public T Add(T item)
     {
         DbEvents.OnPreCreated(item);
         var result = _list.AddLast(item).Value;
         DbEvents.OnCreated(result);
+        Save();
         return result;
+    }
+
+    private IReadOnlyList<T> Find(IsNeed predicate)
+    {
+        return List.Where(predicate.Invoke).ToList();
+    }
+
+    public void Remove(IsNeed predicate)
+    {
+        var candidates = Find(predicate);
+        if (candidates.Count == 0)
+        {
+            DbEvents.OnNotRemoved($"{typeof(T).Name} for deleting not found.");
+        }
+        else
+        {
+            candidates.ToList().ForEach(x =>
+            {
+                DbEvents.OnPreRemoved(x);
+                _list.Remove(x);
+                DbEvents.OnRemoved(x);
+            });
+        }
+        Save();
+    }
+
+    private void Save()
+    {
+        _storage.Save(_list);
+    }
+
+    private void Load()
+    {
+        _list = _storage.Load() ?? [];
     }
 }
