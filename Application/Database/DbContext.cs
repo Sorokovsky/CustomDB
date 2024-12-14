1f4e06627ad8e3527405c99e3dda574e68c74f3d
Sorokovsky <Sorokovskys@ukr.net>
2024-12-14 22:18:14 +0200

    refactor

--- Application/Database/DbContext.cs
+++ Application/Database/DbContext.cs
@@ -5,5 +5,5 @@ namespace Application.Database;
 
 public class DbContext : global::Database.Core.DbContext
 {
-    public Repository<BaseEntity> Entities { get; } = new("Entities");
+    public Repository<BaseEntity> Entities { get; } = new(nameof(Entities));
 }
